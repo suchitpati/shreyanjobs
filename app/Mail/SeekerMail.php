cb4e4f3a3a2adaffe6b9975ce96a1470d9840faa
bhavinsen <senbhavin@gmail.com>
2023-12-08 17:33:03 +0530

    Working on enhancement 5 update

--- app/Mail/SeekerMail.php
+++ app/Mail/SeekerMail.php
@@ -26,9 +26,10 @@ class SeekerMail extends Mailable
     protected $job_title;
     protected $state;
     protected $detailed_description;
+    protected  $cover_letter;
 
 
-    public function __construct($job_title,$fullname,$employername,$city,$country,$additional_detail,$resume,$detailed_description,$state)
+    public function __construct($job_title,$fullname,$employername,$city,$country,$additional_detail,$resume,$detailed_description,$state,$cover_letter)
     {
         $this->job_title = $job_title;
         $this->resume = $resume;
@@ -39,7 +40,7 @@ class SeekerMail extends Mailable
         $this->state = $state;
         $this->country = $country;
         $this->additional_detail = $additional_detail;
-
+        $this->cover_letter =  $cover_letter;
 
 
 
@@ -54,6 +55,6 @@ class SeekerMail extends Mailable
     {
 
 
-        return $this->view('applyJobEmail')->with(['job_title'=>$this->job_title,'country'=>$this->country,'fullname'=>$this->fullname,'employername'=>$this->employername,'city'=>$this->city,'additional_detail'=>$this->additional_detail,'state'=>$this->state,'detailed_description'=>$this->detailed_description])->attach($this->resume);
+        return $this->view('applyJobEmail')->with(['job_title'=>$this->job_title,'country'=>$this->country,'fullname'=>$this->fullname,'employername'=>$this->employername,'city'=>$this->city,'additional_detail'=>$this->additional_detail,'state'=>$this->state,'detailed_description'=>$this->detailed_description,'cover_letter'=>$this->cover_letter])->attach($this->resume);
     }
 }
