9853c0ce5070b92d6e36db791a7190887b4d459d
bhavinsen <senbhavin@gmail.com>
2024-08-06 14:44:26 +0530

    Working on issue fix

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,4 +1,4 @@
-// const apiUrl = 'http://127.0.0.1:8000/api';
-const apiUrl = 'https://shreyanjobs.com/backend/public/api'; //live
+const apiUrl = 'http://127.0.0.1:8000/api';
+// const apiUrl = 'https://shreyanjobs.com/backend/public/api'; //live
 
 export default apiUrl;
